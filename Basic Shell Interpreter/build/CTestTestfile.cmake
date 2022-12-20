810d7de0a53a097729e84acefce3327cb83e4082
Nathan Redmond <nathanr@nathan-standardpcq35ich92009.(none)>
2022-12-20 12:55:07 -0600

    Test 3

---
+++ Basic Shell Interpreter/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathanr/assignment-2-nathanredmond
+# Build directory: /home/nathanr/assignment-2-nathanredmond/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
